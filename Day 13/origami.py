939c4a7a0ebb531c245f177e1ceb0c6802ca5d7b
Daniel Knight <knightd80@Daniels-MBP.broadband>
2021-12-13 09:44:22 +0000

    Day 13

---
+++ Day 13/origami.py
@@ -0,0 +1,69 @@
+inputFile = open('input.txt', 'r')
+lines = inputFile.read().splitlines()
+
+paper = []
+numberOfHashes = 0
+maxy = 0
+maxx = 0
+
+for line in lines:
+    if "," in line:
+        x, y = line.split(",")
+        if int(x) > maxx:
+            maxx = int(x)
+        if int(y) > maxy:
+            maxy = int(y)
+
+for y in range(maxy+1):
+    row = []
+    for x in range(maxx+1):
+        row.append(".")
+    paper.append(row)
+
+for line in lines:
+    if len(line) == 0:
+        pass
+    elif "fold" in line:
+        temp = line.split()
+        axis, pos = temp[2].split("=")
+        if axis == "y":
+            ycount = 1
+            for y in range(int(pos)+1, len(paper)):
+                xcount = 0
+                for x in paper[y]:
+                    mirroredcell = paper[int(pos) - ycount][xcount]
+                    if x == "#" or mirroredcell == "#":
+                        paper[int(pos) - ycount][xcount] = "#"
+                    else:
+                        paper[int(pos) - ycount][xcount] = "."
+                    xcount += 1
+                ycount += 1
+            for y in range(int(pos), len(paper)):
+                paper.pop()
+            break # only interested in result after first fold in this example
+        elif axis == "x":
+            xcount = 1
+            for x in range(int(pos)+1, len(paper[0])):
+                for y in paper:
+                    currentcell = y[x]
+                    mirroredcell = y[int(pos) - xcount]
+                    if currentcell == "#" or mirroredcell == "#":
+                        y[int(pos) - xcount] = "#"
+                    else:
+                        y[int(pos) - xcount] = "."
+                xcount += 1
+            for x in range(int(pos), len(paper[0])):
+                for y in paper:
+                    y.pop()
+            break # only interested in result after first fold in this example
+    else:
+        x, y = line.split(",")
+        paper[int(y)][int(x)] = "#"
+
+for y in paper:
+    for x in y:
+        if x == "#":
+            numberOfHashes += 1
+
+#print(paper)
+print("Number of Hashes: {};".format(numberOfHashes))
