f246dfcbbbc0d9cef1bf3543e3c12efc12190d78
Daniel Knight <knightd80@Daniels-MBP.broadband>
2021-12-15 16:28:12 +0000

    Day 15

---
+++ Day 15/paths2.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+inputFile = open('input.txt', 'r')
+lines = inputFile.read().splitlines()
+
+caves = []
+
+for line in lines:
+    row = []
+    row[:] = line
+    caves.append(row)
+
+# Scale to full size of cave (5 times bigger in each direction and each cell incremented by 1 on each increase)
+# extend in x direction
+tempcaves = []
+for row in caves:
+    temprow = row.copy()
+    for i in range(4):
+        for index2 in range(len(caves[0])):
+            tempvalue = int(row[index2]) + i + 1
+            while tempvalue > 9:
+                tempvalue -= 9
+            temprow.append(tempvalue)
+    tempcaves.append(temprow)
+caves = tempcaves.copy()
+
+# extend in y direction
+for i in range(4):
+    for row in caves:
+        temprow = row.copy()
+        for index2 in range(len(caves[0])):
+            tempvalue = int(temprow[index2]) + i + 1
+            while tempvalue > 9:
+                tempvalue -= 9
+            temprow[index2] = tempvalue
+        tempcaves.append(temprow)
+caves = tempcaves.copy()
+
+max_val = len(caves)
+
+map=np.ones((max_val,max_val),dtype=int)*np.Infinity
+for index, cave in enumerate(caves):
+    for index2, x in enumerate(cave):
+        map[index,index2] = x
+
+# Use Dijkstra’s algorithm
+#Initialize auxiliary arrays
+distmap=np.ones((max_val,max_val),dtype=int)*np.Infinity
+distmap[0,0]=0
+originmap=np.ones((max_val,max_val),dtype=int)*np.nan
+visited=np.zeros((max_val,max_val),dtype=bool)
+finished = False
+x,y=int(0),int(0)
+count=0
+
+#Loop Dijkstra until reaching the target cell
+while not finished:
+    # move to x+1,y
+    if x < max_val-1:
+        if distmap[x+1,y]>map[x+1,y]+distmap[x,y] and not visited[x+1,y]:
+            distmap[x+1,y]=map[x+1,y]+distmap[x,y]
+            originmap[x+1,y]=np.ravel_multi_index([x,y], (max_val,max_val))
+    # move to x-1,y
+    if x>0:
+        if distmap[x-1,y]>map[x-1,y]+distmap[x,y] and not visited[x-1,y]:
+            distmap[x-1,y]=map[x-1,y]+distmap[x,y]
+            originmap[x-1,y]=np.ravel_multi_index([x,y], (max_val,max_val))
+    # move to x,y+1
+    if y < max_val-1:
+        if distmap[x,y+1]>map[x,y+1]+distmap[x,y] and not visited[x,y+1]:
+            distmap[x,y+1]=map[x,y+1]+distmap[x,y]
+            originmap[x,y+1]=np.ravel_multi_index([x,y], (max_val,max_val))
+    # move to x,y-1
+    if y>0:
+        if distmap[x,y-1]>map[x,y-1]+distmap[x,y] and not visited[x,y-1]:
+            distmap[x,y-1]=map[x,y-1]+distmap[x,y]
+            originmap[x,y-1]=np.ravel_multi_index([x,y], (max_val,max_val))
+
+    visited[x,y]=True
+    dismaptemp=distmap
+    dismaptemp[np.where(visited)]=np.Infinity
+    # now we find the shortest path so far
+    minpost=np.unravel_index(np.argmin(dismaptemp),np.shape(dismaptemp))
+    x,y=minpost[0],minpost[1]
+    if x==max_val-1 and y==max_val-1:
+        finished=True
+    count=count+1
+
+print("The path length is: {}".format(distmap[max_val-1,max_val-1]))
